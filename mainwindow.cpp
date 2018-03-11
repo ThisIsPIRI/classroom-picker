03971c6339f7689c2b1c0af5aa2a340cf6036bb3
busyVision <>
2018-03-11 16:48:17 +0900

    File reading complete

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMessageBox>
+
+#include <fstream>
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
 	ui->setupUi(this);
 	ui->mainLayout->setAlignment(ui->startButton, Qt::AlignHCenter);
@@ -18,7 +22,22 @@ void MainWindow::on_entryEdit_returnPressed() {
 }
 
 void MainWindow::on_startButton_clicked() {
-
+	if(!ui->fileNameEdit->text().isEmpty()) {
+		std::ifstream ifs(ui->fileNameEdit->text().toStdString());
+		if(ifs.good()) {
+			while(ifs.good()) {
+				std::string str;
+				std::getline(ifs, str);
+				entries.push_back(str);
+				ui->entryList->addItem(QString::fromStdString(str));
+			}
+		}
+		else {
+			QMessageBox box;
+			box.setText("Couldn't find the file");
+			box.exec();
+		}
+	}
 }
 
 void MainWindow::on_eraseListButton_clicked() {
